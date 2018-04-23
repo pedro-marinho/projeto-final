6bfa80beae8160cd646774b63569ddc76c85a23c
Pedro <pedrobrunomarinho@gmail.com>
2018-04-23 18:51:42 -0300

    two-node synchronization

---
+++ final-node/final-node.ino
@@ -0,0 +1,96 @@
+/* 
+ *  NÓ MAIS AFASTADO (FINAL)
+ *  ENVIA UM POST COM O VALOR DO SEU SENSOR PARA O NÓ SEGUINTE.
+ *  
+*/
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+// Login e senha do nó seguinte
+const char* ssid = "node_two";
+const char* password = "a12345678";
+
+// Verifica se já conseguiu realizar a primeira conexão com o nó seguinte.
+int firstConnection = 0;
+
+// Verifica se conseguiu comunicar durante o tempo acordado.
+int connectionMade = 0;
+
+// Para contar tempo acordado
+int initTime = 0;
+
+// Envia o valor do sensor para o nó seguinte
+void sendMessage(){
+  if (WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+    http.begin("http://192.168.4.1");
+    http.addHeader("Content-Type", "application/json");
+    int httpCode = http.POST("{\"Value\": 150}");
+    Serial.print("HTTP Code: ");
+    Serial.println(httpCode);
+    firstConnection = 1;
+    connectionMade = 1;
+    
+    // String payload = http.getString();
+    http.end();
+  }
+}
+ 
+void setup () { 
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.println("###################################################################################################");
+  Serial.println("AWAKE!");
+}
+
+void loop() {
+  // Fica acordado "pra sempre" até fazer a primeira conexão. Depois, fica acordado por 30 segundos.
+  if((millis() - initTime < 30000 || firstConnection == 0) && connectionMade == 0){
+    sendMessage();
+  } else {
+    if(connectionMade == 1){
+      Serial.println("Conseguiu connectar!!!");
+    } else {
+      Serial.println("NÃO conseguiu conectar...");
+    }
+    Serial.println("Sleeping ...");
+  
+    // Modem Sleep: dorme por 20 segundos.
+    WiFi.forceSleepBegin();
+    delay(20000);
+    WiFi.forceSleepWake();
+
+    Serial.println("###################################################################################################");
+    Serial.println("AWAKE!");
+    initTime = millis();
+    connectionMade = 0;
+  }
+}
+ 
+//void loop() {
+//  Serial.println("###################################################################################################");
+//  Serial.println("AWAKE!");
+//  int initTime = millis();
+//  connectionMade = 0;
+//
+//  // Fica acordado "pra sempre" até fazer a primeira conexão. Depois, fica acordado por 30 segundos.
+//  while((millis() - initTime < 30000 || firstConnection == 0) && connectionMade == 0){
+//    sendMessage();
+//    delay(0);
+//  }
+//
+//  if(connectionMade == 1){
+//    Serial.println("Conseguiu connectar!!!");
+//  } else {
+//    Serial.println("NÃO conseguiu conectar...");
+//  }
+//  
+//  Serial.println("Sleeping ...");
+//  
+//  // Modem Sleep: dorme por 20 segundos.
+//  WiFi.forceSleepBegin();
+//  delay(20000);
+//  WiFi.forceSleepWake();
+//}
---
+++ initial-node/initial-node.ino
@@ -0,0 +1,78 @@
+/*
+ * NÓ PRINCIPAL (INICIAL)
+ * VAI ENVIAR A MENSAGEM RECEBIDA PARA O SERVIDOR.
+ * 
+ */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+
+// Login e senha do nó (access point)
+const char *ssid = "node_two";
+const char *password = "a12345678";
+
+// Verifica se a conexão já foi feita
+int connectionMade = 0;
+
+// Para contar tempo acordado
+int initTime = 0;
+
+// Endereço de IP fixo para o nó na rede dele (access point)
+IPAddress apIP(192, 168, 4, 1);  
+
+ESP8266WebServer server(80);
+
+void response() {
+  Serial.println(server.arg("plain"));
+  connectionMade = 1;
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAP(ssid, password);
+
+  server.on("/", response);
+  server.begin();
+
+  Serial.println("###################################################################################################");
+  Serial.println("AWAKE!");
+}
+
+void loop() {
+  if(millis() - initTime < 30000 && connectionMade == 0){ // Fica acordado por 30 segundos.
+    server.handleClient();
+  } else {
+    Serial.println("Sleeping ...");
+    
+    // Modem Sleep: dorme por 20 segundos.
+    WiFi.forceSleepBegin();
+    delay(20000);
+    WiFi.forceSleepWake();
+    
+    Serial.println("###################################################################################################");
+    Serial.println("AWAKE!");
+    initTime = millis();
+    connectionMade = 0;
+  }
+}
+
+//void loop() {
+//  Serial.println("###################################################################################################");
+//  Serial.println("AWAKE!");
+//  initTime = millis();
+//  connectionMade = 0;
+//
+//  // Fica acordado por 30 segundos.
+//  while(millis() - initTime < 30000 && connectionMade == 0){
+//    server.handleClient();
+//  }
+//  
+//  Serial.println("Sleeping ...");
+//  
+//  // Modem Sleep: dorme por 20 segundos.
+//  WiFi.forceSleepBegin();
+//  delay(20000);
+//  WiFi.forceSleepWake();
+//}
