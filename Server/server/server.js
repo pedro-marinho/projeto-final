d9e39b3a899482cbdf63a323126bcd3dab93dff9
Pedro <pedrobrunomarinho@gmail.com>
2018-05-23 21:47:32 -0300

    get /values e get /sensors

--- Server/server/server.js
+++ Server/server/server.js
@@ -1,31 +1,58 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {Value} = require('./models/valueModel');
+const { Value } = require('./models/valueModel');
 
 const app = express();
-
 app.use(bodyParser.json());
 
 app.post('/values', (req, res) => {
-    var values = [];
+    try {
+        var values = [];
 
-    req.body.forEach(v => {
-        values.push(new Value({
-            value: v.value,
-            sensor: v.sensor,
-            time: v.time
-        }));
-    });
+        req.body.forEach(v => {
+            values.push(new Value({
+                value: v.value,
+                sensor: v.sensor,
+                time: v.time
+            }));
+        });
 
-    Value.collection.insert(values, (e, docs) => {
-        if(e) {
-            res.sendStatus(404);
-            return;
-        }
+        Value.collection.insert(values, (e, docs) => {
+            if (e) {
+                res.sendStatus(400);
+                return;
+            }
 
-        res.sendStatus(200);
-    });
+            res.sendStatus(200);
+        });
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(400);
+    }
+});
+
+app.get('/values', async (req, res) => {
+    try {
+        var values = await Value.find();
+        res.send({data: values});
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(400);
+    }
+});
+
+app.get('/sensors', async (req, res) => {
+    try {
+        var values = await Value.find();
+        var sensors = new Set();
+
+        values.forEach(v => sensors.add(v.sensor));
+        res.send({data: [...sensors]});
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(400);
+    }
 });
 
 app.listen(3000, '192.168.0.11', () => {
