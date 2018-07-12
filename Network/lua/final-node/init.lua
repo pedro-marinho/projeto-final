e58048623e5ead499c5f542a09e23022e3567c48
Pedro <pedrobrunomarinho@gmail.com>
2018-07-12 00:14:29 -0300

    final node lua

--- Network/lua/final-node/init.lua
+++ Network/lua/final-node/init.lua
@@ -15,18 +15,11 @@ end
 -- Configurando modo cliente
 local wificonf = {
   -- verificar ssid e senha
-  ssid = "PEDRO",
-  pwd = "MARCELOM",
+  ssid = "middle_node1",
+  pwd = "a12345678",
   -- callback que vai executar qdo ganhar IP na rede:
-  -- got_ip_cb = function (iptable) print ("ip: ".. iptable.IP); send_message(); end,
+  got_ip_cb = function (iptable) print ("ip: ".. iptable.IP); send_message(); end,
   save = false
 }
 wifi.setmode(wifi.STATIONAP)
 wifi.sta.config(wificonf)
-
-print("COMECOU")
-while true do
-    if wifi.sta.getip() ~= nil then
-        print(wifi.sta.getip())
-    end
-end
